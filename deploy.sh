3afbff8b45ed40640510ddb279f9269693b5e3ae
Haixiang6123 <haixiang6123@gmail.com>
2019-03-31 13:09:46 -0700

    Update deploy files

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:Haixiang6123/a-cat-a-dog-vue.git master:gh-pages
+git push -f git@github.com:haixiang6123/a-cat-a-dog-vue.git master:gh-pages
 
 cd -
