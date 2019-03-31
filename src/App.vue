bf84fed8389a55585bc5ac9f4691fc88e2dfe68d
Haixiang6123 <haixiang6123@gmail.com>
2019-03-31 13:42:02 -0700

    Add basic layout

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <div id="app">
+        <md-card class="app-wrapper">
+            <router-view/>
+        </md-card>
     </div>
-    <router-view/>
-  </div>
 </template>
 
 <style lang="scss">
+    #app {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+
+        .app-wrapper {
+            width: 370px;
+            height: 680px;
+            padding: 16px 8px;
+            overflow: auto;
+        }
+
+        a {
+            text-decoration: none;
+        }
+    }
 </style>
