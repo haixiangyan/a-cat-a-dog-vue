c9f97490228e16b457b8737ccd5df6e3cc753884
Haixiang6123 <haixiang6123@gmail.com>
2019-03-31 17:11:23 -0700

    Add Setting page

--- src/views/Setting.vue
+++ src/views/Setting.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        setting
+    <div class="setting">
+        <Header></Header>
+        <h3>Which one you prefer?</h3>
+        <el-radio :value="user.type" @change="onChangeType" label="CAT">CAT</el-radio>
+        <el-radio :value="user.type" @change="onChangeType" label="DOG">DOG</el-radio>
     </div>
 </template>
 
 <script>
+  import {mapState, mapMutations} from 'vuex'
+  import Header from '../components/Header'
+
   export default {
-    name: "Setting"
+    name: "Setting",
+    computed: {
+      ...mapState(["user"])
+    },
+    methods: {
+      onChangeType(event) {
+        const newUser = {
+          ...this.user,
+          type: event.target.value
+        }
+        // Change localStorage
+        localStorage.setItem('user', JSON.stringify(newUser))
+        // Change store
+        this.updateUser(newUser)
+        this.updateAxios(event.target.value)
+      },
+      ...mapMutations(["updateAxios", "updateUser"])
+    },
+    components: {
+      Header
+    }
   }
 </script>
 
 <style scoped>
-
+    .setting {
+        padding: 8px 16px;
+    }
 </style>
